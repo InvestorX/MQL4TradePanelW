82485e75d54b91a5748b80497b338606fc47e8d3
InestroX <kinoco11+git@gmail.com>
2022-02-14 12:04:03 +0900

    プロジェクト ファイルを追加します。

---
+++ MQL4TradePanelW/BuyButton.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MQL4TradePanelW
+{
+
+    //referer https://blogs.jp.infragistics.com/entry/2019/06/28/182558
+
+    /// <summary>
+    /// このカスタム コントロールを XAML ファイルで使用するには、手順 1a または 1b の後、手順 2 に従います。
+    ///
+    /// 手順 1a) 現在のプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    /// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    /// 追加します:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:MQL4TradePanelW"
+    ///
+    ///
+    /// 手順 1b) 異なるプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    /// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    /// 追加します:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:MQL4TradePanelW;assembly=MQL4TradePanelW"
+    ///
+    /// また、XAML ファイルのあるプロジェクトからこのプロジェクトへのプロジェクト参照を追加し、
+    /// リビルドして、コンパイル エラーを防ぐ必要があります:
+    ///
+    ///     ソリューション エクスプローラーで対象のプロジェクトを右クリックし、
+    ///     [参照の追加] の [プロジェクト] を選択してから、このプロジェクトを参照し、選択します。
+    ///
+    ///
+    /// 手順 2)
+    /// コントロールを XAML ファイルで使用します。
+    ///
+    ///     <MyNamespace:MaterialDesignButton/>
+    ///
+    /// </summary>
+    public class BuyButton :Button
+    {
+        static BuyButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BuyButton), new FrameworkPropertyMetadata(typeof(BuyButton)));
+        }
+    }
+}
