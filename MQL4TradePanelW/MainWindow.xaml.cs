081c3a6682562674d5ac0737ca0f502fbff935f0
InestroX <kinoco11+git@gmail.com>
2022-02-14 14:03:27 +0900

    update

--- MQL4TradePanelW/MainWindow.xaml.cs
+++ MQL4TradePanelW/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +26,60 @@ namespace MQL4TradePanelW
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        [DllImport("MemMap.dll", CharSet = CharSet.Ansi)]
+        private extern static IntPtr GetMemString(String tag);
+
+        [DllImport("MemMap.dll", CharSet = CharSet.Ansi)]
+        private extern static IntPtr SetMemString(string tag, string msg);
+
+
+        private void lots_PreviewMouseDown(object sender, RoutedEventArgs e)
+        {
+            Trace.WriteLine("PreviewMouseDown lotsがクリックされた気がする");
+            //popupLots.IsOpen = !popupLots.IsOpen;
+
+        }
+
+        private void lots_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Trace.WriteLine("PreviewMouseUP lotsがクリックされた気がする");
+            popupLots.IsOpen = !popupLots.IsOpen;
+
+        }
+
+        private void sellBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void popupBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Trace.WriteLine(((Button)sender).Content.ToString());
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            popupLots.IsOpen = false;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            TimerCallback timerDelegate = new TimerCallback(requestMTStatus);
+            Timer timer = new Timer(timerDelegate, null, 0, 200);
+        }
+
+        private void requestMTStatus(object state)
+        {
+            try
+            {
+                GetMemString("MT2EXE");
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.ToString());
+            }
         }
     }
 }
